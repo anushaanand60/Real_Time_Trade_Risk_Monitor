f175c5a4a4fc6dbf197273d02f08da4e97c5d57e
Anusha Anand <anandanusha610@gmail.com>
2026-05-20 22:20:25 +0530

    adding input and output schema: portfolio

---
+++ trade-risk-monitor/app/schemas/portfolio.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class PortfolioCreate(BaseModel):
+    name: str
+
+
+class PortfolioResponse(BaseModel):
+    id: int
+    name: str
+    created_at: datetime
+
+    model_config = {"from_attributes": True}
