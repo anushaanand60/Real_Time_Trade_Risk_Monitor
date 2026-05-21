a6f9699ac562d5110a004bca9d04211c0de3edc9
Anusha Anand <anandanusha610@gmail.com>
2026-05-21 20:35:44 +0530

    adding os.getenv('TESTING') guard to prevent app from trying to connect to production database during tests

--- trade-risk-monitor/app/main.py
+++ trade-risk-monitor/app/main.py
@@ -1,9 +1,11 @@
+import os
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.routers.portfolio import router as portfolio_router
 from app.routers.trade import router as trade_router
 
-Base.metadata.create_all(bind=engine)
+if os.getenv("TESTING") != "True":
+    Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(portfolio_router)
